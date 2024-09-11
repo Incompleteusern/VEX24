7a03ffa63dc5d076e5957cfbb85afa006df0afa4
royce <royce.yao.ry@Gmail.com>
2024-09-10 21:29:20 -0700

    stuff

---
+++ src/ports.cpp
@@ -0,0 +1,56 @@
+
+#include "main.h"
+
+#define track_width 12.6
+#define rpm 450
+#define wheel_type lemlib::Omniwheel::NEW_275
+
+pros::MotorGroup left_motors({-1, 2, 3}, pros::MotorGearset::blue); 
+pros::MotorGroup right_motors({10, -9, -8}, pros::MotorGearset::blue); 
+
+lemlib::Drivetrain drivetrain(&left_motors, // left motor group
+                              &right_motors, // right motor group
+                              track_width, // 10 inch track width
+                              wheel_type, // using new 4" omnis
+                              rpm, // drivetrain rpm is 360
+                              2 // horizontal drift is 2 (for now)
+);
+
+// pros::Imu imu(10);
+
+lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+                            nullptr, // horizontal tracking wheel 1
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            nullptr // &imu // inertial sensor
+);
+
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+lemlib::Chassis chassis(drivetrain, // drivetrain settings
+                        lateral_controller, // lateral PID settings
+                        angular_controller, // angular PID settings
+                        sensors // odometry sensors
+);
